ba942b0497b32179f0ae5cf9b60816e17b0a4f5a
miss-monkey <justina278@gmail.com>
2021-03-18 12:00:52 +0000

    created quantity form

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404
+)
+
+from products.models import Product
 
 
 def view_bag(request):
@@ -10,6 +14,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add quantity of specified product to the bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -21,3 +26,20 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
