d99bcfb811f8b6057c02867aba0bc70bb67e02db
miss-monkey <justina278@gmail.com>
2021-03-21 06:59:31 +0000

    Revoked secret key and set debug

--- vase_e_shop/settings.py
+++ vase_e_shop/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$lw#bpaxve%0p9j$t-1&$@82^2ix_3j-_h3$9p)hh*5u-eb@@*'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['vase-e-store.herokuapp.com', 'localhost']
 
